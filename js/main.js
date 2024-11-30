2ba1f8557dcd99326366c478fad41a5a729777b3
Chandrakanthkokkiripati <chandrakanthkokkiripati1998@gmail.com>
2024-11-30 11:14:53 +0530

    c

--- js/main.js
+++ js/main.js
@@ -326,6 +326,25 @@ Version:	1.1
 	$(window).on('load', function() {
 		$('.preloader').addClass('preloader-deactivate');
 	});
+
+	document.addEventListener("DOMContentLoaded", () => {
+		const tabButtons = document.querySelectorAll(".tab-button");
+		const tabContents = document.querySelectorAll(".tab-content");
+	  
+		tabButtons.forEach((button) => {
+		  button.addEventListener("click", () => {
+			// Remove active class from all buttons and contents
+			tabButtons.forEach((btn) => btn.classList.remove("active"));
+			tabContents.forEach((content) => content.classList.remove("active"));
+	  
+			// Add active class to clicked button and respective content
+			button.classList.add("active");
+			const tabId = button.getAttribute("data-tab");
+			document.getElementById(tabId).classList.add("active");
+		  });
+		});
+	  });
+	  
 	
 	
 })(jQuery);
